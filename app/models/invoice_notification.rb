592bcca62268157db890ea202e057f7e9f2990aa
mobileAgent <flester@gmail.com>
2015-10-27 23:34:34 -0400

    update where to send checks, fix invoice email

--- app/models/invoice_notification.rb
+++ app/models/invoice_notification.rb
@@ -1,14 +1,13 @@
 class InvoiceNotification < ActionMailer::Base
 
   def invoice(user,registration,main_event)
-     recipients  user.email
-     # @from = '"Florida Bible Study" <info@floridabiblestudy.org>'
-     from  'info@floridabiblestudy.org'
-     bcc  'info@floridabiblestudy.org'
-     sent_on  Time.now
-     subject  "Your invoice for Florida Mens Bible Study #{main_event.year}"
-     headers  {}
-     body  make_invoice_text(user,registration,main_event)
+    @recipients = user.email
+    @from = 'info@floridabiblestudy.org'
+    @bcc = 'info@floridabiblestudy.org'
+    @sent_on = Time.now
+    @subject = "Your invoice for Florida Mens Bible Study #{main_event.year}"
+    @headers = {}
+    @body = make_invoice_text(user,registration,main_event)
   end
 
   private
@@ -28,16 +27,16 @@ class InvoiceNotification < ActionMailer::Base
 
     p2 += "Amount due: $#{registration.amount_owed}.00\n"
     
-    if DateTime.now > (main_event.start_date - 30)
-      p2 += "The event is too close to pay by check.\n"
-      p2 += "Please pay the full amount in cash on the first\n"
-      p2 += "day of the study.\n"
+    if DateTime.now > (main_event.start_date - 10)
+      p2 += "The event is too close to pay by mail.\n"
+      p2 += "Please bring your cash or check to the study \n"
+      p2 += "and give it to Mike Renth. Thanks!\n"
      else
-       p2 += "You can make your check payable to Mike Flester\n"
+       p2 += "You can make your check payable to *Boulevard Bible Chapel*\n"
        p2 += "And mail it to \n\n"
-       p2 += "                 Florida Mens Bible Study c/o Mike Flester\n"
-       p2 += "                 7113 Deer Valley Road\n"
-       p2 += "                 Highland, MD 20777 USA\n"
+       p2 += "                 Florida Mens Bible Study c/o Mike Renth\n"
+       p2 += "                 2481 SW 99th Way\n"
+       p2 += "                 Miramar FL 33025 USA\n"
      end
        
      p3 = "Thank you for registering for the #{main_event.year} Study,\n"
